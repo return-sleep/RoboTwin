888d6136ba47ed7505808cca26e3d273146b6db0
xhl <21110720074@m.fudan.edu.cn>
2025-03-25 19:42:05 +0800

    minor change

--- script/test_render.py
+++ script/test_render.py
@@ -1,12 +1,13 @@
 import sys
 import warnings
 import os
-warnings.simplefilter(action='ignore', category=FutureWarning)
-warnings.simplefilter(action='ignore', category=UserWarning)
+
+warnings.simplefilter(action="ignore", category=FutureWarning)
+warnings.simplefilter(action="ignore", category=UserWarning)
 current_file_path = os.path.abspath(__file__)
 parent_dir = os.path.dirname(current_file_path)
 
-sys.path.append(os.path.join(parent_dir, '../../tools'))
+sys.path.append(os.path.join(parent_dir, "../../tools"))
 import numpy as np
 import pdb
 import json
@@ -23,12 +24,13 @@ import random
 import sys
 import warnings
 import os
-warnings.simplefilter(action='ignore', category=FutureWarning)
-warnings.simplefilter(action='ignore', category=UserWarning)
+
+warnings.simplefilter(action="ignore", category=FutureWarning)
+warnings.simplefilter(action="ignore", category=UserWarning)
 current_file_path = os.path.abspath(__file__)
 parent_dir = os.path.dirname(current_file_path)
 
-sys.path.append(os.path.join(parent_dir, '../../tools'))
+sys.path.append(os.path.join(parent_dir, "../../tools"))
 import numpy as np
 import pdb
 import json
@@ -41,33 +43,35 @@ import transforms3d as t3d
 from collections import OrderedDict
 import random
 
+
 class Sapien_TEST(gym.Env):
     def __init__(self):
         super().__init__()
-        ta.setup_logging("CRITICAL") # hide logging
+        ta.setup_logging("CRITICAL")  # hide logging
 
         self.setup_scene()
-        print('render ok')
-        # try: 
+        print("render ok")
+        # try:
         #     self.setup_scene()
         #     print('render ok')
         # except:
         #     print('render error')
         #     exit()
 
-    def setup_scene(self,**kwargs):
-        '''
+    def setup_scene(self, **kwargs):
+        """
         Set the scene
             - Set up the basic scene: light source, viewer.
-        '''
+        """
         self.engine = sapien.Engine()
         # declare sapien renderer
         from sapien.render import set_global_config
-        set_global_config(max_num_materials = 50000, max_num_textures = 50000)
+
+        set_global_config(max_num_materials=50000, max_num_textures=50000)
         self.renderer = sapien.SapienRenderer()
         # give renderer to sapien sim
         self.engine.set_renderer(self.renderer)
-        
+
         sapien.render.set_camera_shader_dir("rt")
         sapien.render.set_ray_tracing_samples_per_pixel(32)
         sapien.render.set_ray_tracing_path_depth(8)
@@ -77,5 +81,6 @@ class Sapien_TEST(gym.Env):
         scene_config = sapien.SceneConfig()
         self.scene = self.engine.create_scene(scene_config)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     a = Sapien_TEST()
