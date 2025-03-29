aa57660eea7f068e192acad13c09854c2ff36001
xhl <21110720074@m.fudan.edu.cn>
2025-03-29 19:13:11 +0800

    minor change

--- policy/ACT-DP-TP/scripts/act_dp_tp/train.sh
+++ policy/ACT-DP-TP/scripts/act_dp_tp/train.sh
@@ -21,10 +21,10 @@ lr_schedule_type=cosine_warmup
 # ['blocks_stack_hard', 'block_handover', 'bottle_adjust', 'container_place', 'diverse_bottles_pick', 'dual_bottles_pick_easy', 'dual_bottles_pick_hard', 'block_hammer_beat', 'block_handover', 'blocks_stack_easy', 'dual_shoes_place', 'empty_cup_place', 'mug_hanging_easy', 'mug_hanging_hard', 'put_apple_cabinet', 'shoe_place','pick_apple_messy']
 attention_type=v3
 echo "Processing $task_name"
-python3 train_policy_robotwin.py \
+CUDA_VISIBLE_DEVICES=3 python3 train_policy_robotwin.py \
     --task_name  $task_name \
     --ckpt_dir checkpoints/$task_name/act_dp_tp_${chunk_size}_${predict_frame}_${temporal_downsample_rate}_${tokenizer_model_temporal_rate}_${lr_schedule_type}/seed_$seed/num_epochs_$num_epochs \
-    --policy_class ACT_diffusion_tp --hidden_dim 512  --batch_size 16 --dim_feedforward 3200 \
+    --policy_class ACT_diffusion_tp --hidden_dim 512  --batch_size 256 --dim_feedforward 3200 \
     --chunk_size $chunk_size  --norm_type minmax --disable_vae_latent \
     --predict_frame $predict_frame --patch_size 5 --temporal_downsample_rate $temporal_downsample_rate   --prediction_weight 0.2  \
     --tokenizer_model_temporal_rate $tokenizer_model_temporal_rate --tokenizer_model_spatial_rate 8 \
