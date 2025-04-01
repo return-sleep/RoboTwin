27e186b5ef7dc56264eb56e27a908151797dd360
xuhuilin <21110720074@m.fudan.edu.cn>
2025-04-01 20:18:29 +0800

    add multi-view coda for act_dp

--- data_generate.sh
+++ data_generate.sh
@@ -1,12 +1,5 @@
-for task_name in put_bottles_dustbin dual_shoes_place bowls_stack blocks_stack_hard; do
+for task_name in  bowls_stack blocks_stack_hard; do
+    echo $task_name
     bash run_task.sh $task_name 0  
-    python script/pkl2zarr_mypolicy.py $task_name 100
-    cd data/data_zarr
-    tar -czf ${task_name}_D435_100.zarr.tar.gz ${task_name}_D435_100.zarr  
-    scp -P 20176 ${task_name}_D435_100.zarr.tar.gz xhl@10.176.54.116:/attached/remote-home2/xhl/8_kaust_pj/RoboTwin/data
-    rm -rf ${task_name}_D435_100.zarr.tar.gz
-    cd ..
-    rm -rf ${task_name}_D435_100.zarr  
-    cd ..
-    rm -rf ${task_name}_D435_100
+    scp -P 20088 -r data/${task_name}_D435 xhl@10.176.54.118:/attached/remote-home2/xhl/8_kaust_pj/RoboTwin/data/
 done
